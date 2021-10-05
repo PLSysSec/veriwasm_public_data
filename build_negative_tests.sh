37c3cb4245735e90dd397a627df18c865fd5a722
enjhnsn2 <e5johnso@eng.ucsd.edu>
2021-10-05 13:40:57 -0500

    extra negative tests

--- build_negative_tests.sh
+++ build_negative_tests.sh
@@ -1,3 +1,6 @@
 #build negative tests
 nasm negative_tests/negative_tests.S -f elf64 -o negative_tests/negative_tests.o -w-number-overflow
 gcc negative_tests/negative_tests.o -o negative_tests/negative_tests.so -nostartfiles -nostdlib -nodefaultlibs -shared
+# Build negative tests for zerocost checker
+nasm negative_tests/negative_tests_locals.S -f elf64 -o negative_tests/negative_tests_locals.o -w-number-overflow
+gcc negative_tests/negative_tests_locals.o -o negative_tests/negative_tests_locals.so -nostartfiles -nostdlib -nodefaultlibs -shared
