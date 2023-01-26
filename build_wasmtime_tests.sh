7aa373bfe350b6aef40cdb10d0aa900cae50e18d
Evan Johnson <e5johnso@eng.ucsd.edu>
2023-01-26 11:28:15 -0800

    tiny fix

--- build_wasmtime_tests.sh
+++ build_wasmtime_tests.sh
@@ -1,3 +1,2 @@
-cd wasmtime/build_wasmtime/ && cargo run -- ../../../../wasmtime/target/debug/wasmtime ../tests/wasm x86_64-unknown-linux
-cd wasmtime/build_wasmtime/ && cargo run -- ../../../../wasmtime/target/debug/wasmtime ../tests/wat x86_64-unknown-linux
+cd wasmtime/build_wasmtime/ && cargo run -- ../../../../wasmtime/target/debug/wasmtime ../tests/wasm x86_64-unknown-linux && cargo run -- ../../../../wasmtime/target/debug/wasmtime ../tests/wat x86_64-unknown-linux
 
